ba90e82957229b6910fd1c531296e21dcbcfcc4d
XuHo-IT <ngotranxuanhoa09062004@gmail.com>
2025-07-06 10:21:43 +0700

    upload feauture_AIChat

---
+++ Application/Request/Chat/ChatRequest.cs
@@ -0,0 +1,10 @@
+﻿using Application.Entities.Base;
+
+namespace Application.Request.Chat
+{
+    public class ChatRequest
+    {
+        public string Message { get; set; }
+        public List<Message>? PreviousMessages { get; set; }
+    }
+}
---
+++ EcoGreen/Service/Chat/AIChatAPIClient.cs
@@ -0,0 +1,36 @@
+﻿using Application.Entities.Base;
+using Application.Response.Chat;
+
+namespace EcoGreen.Service.Chat
+{
+    public class AIChatAPIClient : IDisposable
+    {
+        private readonly HttpClient _client;
+
+        public AIChatAPIClient()
+        {
+            _client = new HttpClient
+            {
+                BaseAddress = new Uri("http://localhost:11434")
+            };
+        }
+
+        public async Task<AIResponse> GenerateChatAsync(ChatModel model)
+        {
+            var payload = new
+            {
+                model = model.Model,
+                messages = model.Messages.Select(m => new { role = m.Role, content = m.Content }),
+                stream = false
+            };
+
+            var response = await _client.PostAsJsonAsync("/api/chat", payload);
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadFromJsonAsync<AIResponse>();
+        }
+
+        public void Dispose() => _client.Dispose();
+    }
+}
+
